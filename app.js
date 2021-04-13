c349792d7afdc924282b068b90435f0c0e3fe3e1
Mohamed Hassan <mohamedahassan188@gmail.com>
2021-04-13 17:53:55 +0100

    Hash authentication

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -17,10 +16,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
 const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -42,7 +37,7 @@ app.listen(3000, () => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   newUser.save((err) => {
     if (err) console.log("There's an error");
@@ -56,7 +51,7 @@ app.post("/login", (req, res) => {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === req.body.password) {
+        if (foundUser.password === md5(req.body.password)) {
           res.render("secrets");
         }
       }
