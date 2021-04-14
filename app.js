2efde2bbf6e57aed5e96827f674e7e3f121ba037
Mohamed Hassan <mohamedahassan188@gmail.com>
2021-04-14 14:49:27 +0100

    update googlestrategy

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.use(
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      User.findOrCreate({ username: profile.id }, function (err, user) {
         return cb(err, user);
       });
     }
